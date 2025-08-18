7309110361ba3c3537048c5abf3ff7a8cd43a989
tiagomdr <tiago335tm@gmail.com>
2025-08-18 20:27:19 -0300

    testando projeto jan doces fase 1.0

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const AparecerTabela = document.querySelector('.img-valores')
+const escuro = document.querySelector('.escuro')
+function tabela() {
+
+    AparecerTabela.style.left = "40%"
+    AparecerTabela.style.transition = "1s ease-in-out"
+    escuro.style.visibility = "visible"
+}
+
+
+function desaparecer() {
+    AparecerTabela.style.left = "-500px"
+    AparecerTabela.style.transition = "1s ease-in-out"
+    escuro.style.visibility = "hidden"
+    escuro.style.transition = "1s ease-in-out"
+}
