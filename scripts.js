3dd0196cce837be346389cdf86f7ba08ff659963
tiagomdr <tiago335tm@gmail.com>
2025-08-18 20:43:16 -0300

    MOD

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const AparecerTabela = document.querySelector('.img-valores')
 const escuro = document.querySelector('.escuro')
 function tabela() {
 
-    AparecerTabela.style.left = "40%"
+    AparecerTabela.style.left = "30%"
     AparecerTabela.style.transition = "1s ease-in-out"
     escuro.style.visibility = "visible"
 }
