253c8b2259329689567c97b276911254920f917b
tiagomdr <tiago335tm@gmail.com>
2025-08-18 23:30:14 -0300

    1.5

--- scripts.js
+++ scripts.js
@@ -2,7 +2,12 @@ const AparecerTabela = document.querySelector('.img-valores')
 const escuro = document.querySelector('.escuro')
 function tabela() {
 
-    AparecerTabela.style.left = "30%"
+    if(window.innerWidth <= 768) {
+        AparecerTabela.style.left = "15%"
+    }else {
+         AparecerTabela.style.left = "40%"
+    }
+    
     AparecerTabela.style.transition = "1s ease-in-out"
     escuro.style.visibility = "visible"
 }
@@ -13,4 +18,19 @@ function desaparecer() {
     AparecerTabela.style.transition = "1s ease-in-out"
     escuro.style.visibility = "hidden"
     escuro.style.transition = "1s ease-in-out"
+}
+
+
+function enviarWhatsapp() {
+    const nome = document.querySelector('#nome').value
+    const email = document.querySelector('#email').value
+    const select = document.querySelector('select').value
+    const whatsapp = document.querySelector('#whatsapp').value
+
+    const msg  = `Olá! Me chamo ${nome}. Gostaria de solicitar um    ${select}\nContato:${whatsapp}\nEmail:${email}`
+    const numero = '53997098651'
+
+    const url = `https://wa.me/${numero}?text=${encodeURIComponent(msg)}`
+
+    window.open(url, '_blank')
 }
